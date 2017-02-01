373fbe85d419237c418035f80d83beca0cffc265
Colin W Cambo <colinwc0@gmail.com>
2017-02-01 18:16:22 -0500

    reformatted script

--- setup.py
+++ setup.py
@@ -15,62 +15,70 @@ import sqlite3
 import pandas as pd
 from dateutil.parser import parse
 import os
-from progress_bar import MsgBar
+import pyprind
 import warnings
+import time
 warnings.filterwarnings('ignore')
-#import argparse
 
 DATA_PATH = "./Data/WiFi_Data/"
 CONVERT_TIME = lambda x: parse(x.replace('+00', ''))
 
 def run():
+    
+    t0 = time.time()
     file_names = [name for name in os.listdir(DATA_PATH) if name.endswith('.csv')]
     
-    bar = MsgBar(message='Reading in files:', max=len(file_names))
+    print('---------------------------------------------------')
+    print('-----------------Reading in files:-----------------')
+    print('---------------------------------------------------')
+    p = pyprind.ProgBar(50, width=200)
+    
     wifi_df = pd.DataFrame()
     for name in file_names:
         
-        bar.next(message='Reading in: {:20}'.format(name))
         df = pd.read_csv(DATA_PATH+name, names=['MAC_Address', 'Time', 'Building'],
                          converters={'Time':CONVERT_TIME}, header=0)
         wifi_df = pd.concat([wifi_df, df])
+        p.update(iterations=50/len(file_names))
         
-    bar.finish()
+    print('---------------------------------------------------')
+    print('-----------------Extracting Time:-----------------')
+    print('---------------------------------------------------')
+    p = pyprind.ProgBar(50, width=200)
     
-    bar = MsgBar(message='Extracting Hour Column   ', max=3)
-    #Appending new time columns to DataFrame
-    bar.next()
     wifi_df['Hour'] = [time.hour for time in wifi_df.Time]
-    bar.next(message='Extracting Weekday Column')
+    p.update(iterations=16)
     wifi_df['Weekday'] = [time.weekday() for time in wifi_df.Time]
-    bar.next(message='Extracting Minutes Column')
+    p.update(iterations=17)
     wifi_df['Minutes'] = [time.minute for time in wifi_df.Time]
-    bar.finish()
+    p.update(iterations=18)
     wifi_df = wifi_df.sort(columns='Time')
     
-    #print('Putting table into sqlite database!')
-    bar = MsgBar(message='Creating SQLite Database:', max=1)
+    print('---------------------------------------------------')
+    print('-----------------Creating Database:-----------------')
+    print('---------------------------------------------------')
+    p = pyprind.ProgBar(50, width=200)
+    
     con = sqlite3.connect(r'./WiFi_database.sqlite')
     wifi_df.to_sql('wifi_09_19', con)
-    bar.next()
-    bar.finish()
+    p.update(iterations=50)
     
-    #print('Creating SQLite Indexes for faster searching!')
-    #bar = Bar(message='Creating SQLite Indexes:', max=1)
+    
+    print('---------------------------------------------------')
+    print('-----------------Creating Indexes:-----------------')
+    print('---------------------------------------------------')
+    p = pyprind.ProgBar(50, width=200)
+    
     c = con.cursor()
-    bar = MsgBar(message='Creating SQLite Indexes:  ', max=3)
     for column in ['Hour', 'Weekday', 'Building']:
-        bar.next(message='Creating Index for {:10}'.format(column))
         c.execute('CREATE INDEX {ix} on {tn}({cn})'\
             .format(ix=column+'_index', tn='wifi_09_19', cn=column))
-    bar.finish()
+        p.update(iterations=17)
     con.close()
-
+    
+    print("Finished Running Script in {} Seconds!".format(round(time.time() - t0),2))
 if __name__ == "__main__":
     
-    #parser = argparse.ArgumentParser()
-    
-    #parser.add_argument('program', metavar='PROG', type=str)
     run()
     
 
